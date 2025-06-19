7af3b719002d0c98f7a748ee350ead4a187dd1af
Artus <artuslama@gmail.com>
2025-06-19 15:52:45 +0200

    🚨 Add linting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
+    devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ],
+    modules: [
+        "@nuxt/eslint",
+        "@nuxt/fonts",
+        "@nuxt/icon",
+        "@nuxt/image",
+    ],
 
-  future: {
-    compatibilityVersion: 4,
-  }
+    eslint: {
+        config: {
+            stylistic: true,
+        },
+    },
+
+    future: {
+        compatibilityVersion: 4,
+    },
+    compatibilityDate: "2025-05-15",
 })
